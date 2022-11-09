4e221605299dd4042ef4be3273c7a3d238549909
colbyendres <colby.endres@tamu.edu>
2022-11-08 19:38:35 -0600

    Added comments

--- newton.py
+++ newton.py
@@ -9,16 +9,16 @@ def f(x):
 def df(x):
     return  3*x**2-3
 
-def newton(x1,it=0):
+def newton(x1,it=1):
     x2 = x1 - f(x1)/df(x1)
-    print(x1,x2,abs(f(x2)-f(x1)))
+    print('iteration #'+str(it)+": "+str(x1)+'   '+str(x2)+'   '+str(abs(x2-x1)))
     if (abs(f(x2)-f(x1)) < tol):
         return x2
     return newton(x2,it+1)
 
-def bisection(a,b,it=0):
+def bisection(a,b,it=1):
     c = (a+b)/2
-    print("it", it,"",[[a,b],c])
+    print('iteration #'+str(it)+": "+str(a)+'   '+str(b)+'   '+str(abs(f(c))))
     if abs(f(c)) < tol:
         return c
     elif f(a) * f(c) < 0:
@@ -33,8 +33,8 @@ def secant(a,b,it=1):
         return c
     return secant(b,c,it+1)
 
-# print('Bisection:')
-# print(bisection(0,3))
+print('Bisection:')
+print(bisection(0,3))
 
 print('Secant')
 print(secant(0,3))
